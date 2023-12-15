2df22545cde679cf428184a202565b6b1785c841
quanghuyy86 <taquanghuy147@gmail.com>
2023-12-15 17:44:06 +0700

    add @bean

--- medicine-warehouse-service/src/main/java/vn/edu/hau/medicinewarehouse/medicinewarehouseservice/MedicineWarehouseServiceApplication.java
+++ medicine-warehouse-service/src/main/java/vn/edu/hau/medicinewarehouse/medicinewarehouseservice/MedicineWarehouseServiceApplication.java
@@ -2,6 +2,7 @@ package vn.edu.hau.medicinewarehouse.medicinewarehouseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,7 +12,7 @@ public class MedicineWarehouseServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedicineWarehouseServiceApplication.class, args);
 	}
-
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
